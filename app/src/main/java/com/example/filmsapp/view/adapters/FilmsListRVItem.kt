cdb6070974628b3c68747b2fd6350f6cf8d82e0a
Andrew Weiss <andreivayss@gmail.com>
2022-01-06 16:12:25 +0700

    Made design for detailed film screen

--- app/src/main/java/com/example/filmsapp/view/adapters/FilmsListRVItem.kt
+++ app/src/main/java/com/example/filmsapp/view/adapters/FilmsListRVItem.kt
@@ -14,7 +14,7 @@ sealed class FilmsListRVItem {
         @Json(name = "localized_name") val localName: String?,
         val name: String?,
         val year: Int?,
-        val rating: Double?,
+        val rate: Double?,
         @Json(name = "image_url") val imageUrl: String?,
         val description: String?,
         val genres: List<String>?,
